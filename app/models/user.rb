6babc3ecbd27fca1f1e3135a93631e13a234053c
kazusayahazu <kazusaya1025@gmail.com>
2021-10-01 21:39:37 +0900

    商品出品機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/ }
   validates :last_name_kana, :first_name_kana, format: { with: /\p{katakana}/ }
 
-  #has_many :items
+  has_many :items
   #has_many :purchases
 
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,17 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"tarou"}
-    email                 {"sample@gmail.com"}
-    password              {"123abc"}
-    password_confirmation {"123abc"}
+    transient do
+      person { Gimei.name }
+    end
+    nickname              { Faker::Name.name }
+    email                 { Faker::Internet.free_email }
+    password = "a1"+Faker::Internet.password(min_length: 6)
+    password              { password }
+    password_confirmation { password }
     last_name             {"山田"}
     first_name            {"太郎"}
     last_name_kana        {"ヤマダ"}
     first_name_kana       {"タロウ"}
-    birthday              {"2000-01-01"}
+    birthday              { Faker::Date.birthday(min_age:18, max_age:65) }
   end
 end
